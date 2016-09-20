e841cc32dc0785175f9e15dfbb9ff3258026df42
pierdr <pierdr@yahoo.it>
2016-09-20 18:47:00 +0200

    mobile web

--- Web/1 - html-css-js/intelligence.js
+++ Web/1 - html-css-js/intelligence.js
@@ -2,8 +2,15 @@ console.log("hello world");
 
 $(function(){
 
-	$("button").on("click",function(){
-		$("#name").html("world");
+	$("button").on("mouseover",function(){
+		$("#name").css("color","blue","font-size","20");
+		$("#image").attr("src","https://cdn4.vtourist.com/14/3975206.jpg");
+
+
+	});
+
+	$(window).on("resize",function(){
+		console.log("resized");
 	});
 
 
